e1f7b47a08618d864c838a49470ef8e1bc9ce6f8
czzgods <3302076969@qq.com>
2026-09-20 18:15:45 +0800

    --修复部分BUG

--- src/main/java/com/ag/agaicodemother/config/RoutingAiModelConfig.java
+++ src/main/java/com/ag/agaicodemother/config/RoutingAiModelConfig.java
@@ -3,28 +3,33 @@ package com.ag.agaicodemother.config;
 import dev.langchain4j.model.chat.ChatModel;
 import dev.langchain4j.model.openai.OpenAiChatModel;
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
-@ConfigurationProperties(prefix = "langchain4j.open-ai.routing-chat-model")
+//@ConfigurationProperties(prefix = "langchain4j.open-ai.routing-chat-model")
 @Data
 public class RoutingAiModelConfig {
 
+    @Value("${langchain4j.open-ai.routing-chat-model.base-url}")
     private String baseUrl;
 
+    @Value("${langchain4j.open-ai.routing-chat-model.api-key}")
     private String apiKey;
 
+    @Value("${langchain4j.open-ai.routing-chat-model.model-name}")
     private String modelName;
 
+    @Value("${langchain4j.open-ai.routing-chat-model.max-tokens}")
     private Integer maxTokens;
 
-    private Double temperature;
-
+    @Value("${langchain4j.open-ai.routing-chat-model.log-requests}")
     private Boolean logRequests = false;
 
+    @Value("${langchain4j.open-ai.routing-chat-model.log-responses}")
     private Boolean logResponses = false;
 
     /**
@@ -38,7 +43,6 @@ public class RoutingAiModelConfig {
                 .modelName(modelName)
                 .baseUrl(baseUrl)
                 .maxTokens(maxTokens)
-                .temperature(temperature)
                 .logRequests(logRequests)
                 .logResponses(logResponses)
                 .build();
